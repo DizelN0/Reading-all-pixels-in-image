95d8ab9951bf1ea13387880a6ddebcc58ce554ce
DizelN0 <me@revenets.ru>
2023-05-18 00:47:27 +0400

    Added var 'High' and 'Weight'

--- steg.py
+++ steg.py
@@ -1,8 +1,10 @@
 from PIL import Image
 img = Image.open('C:\Program Files\image.png', 'r')
 #Using a loop, we run through each pixel and read it:
-for i in range(70):
-    for j in range(450):
+Weight = int(input('input a weight'))
+High = int(input('input a high'))
+for i in range(High):
+    for j in range(Weight):
         r,g,b = img.getpixel((j,i))
         #Delete all white pixels:
         if not (r == g == b == 255) :
