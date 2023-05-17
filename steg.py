d59fa0c5caddaa937c1dfe40eb553c4b7837b0fd
DizelN0 <me@revenets.ru>
2023-05-18 00:49:35 +0400

    reformat the code

--- steg.py
+++ steg.py
@@ -1,13 +1,12 @@
 from PIL import Image
+
 img = Image.open('C:\Program Files\image.png', 'r')
-#Using a loop, we run through each pixel and read it:
+# Using a loop, we run through each pixel and read it:
 Weight = int(input('input a weight'))
 High = int(input('input a high'))
 for i in range(High):
     for j in range(Weight):
-        r,g,b = img.getpixel((j,i))
-        #Delete all white pixels:
-        if not (r == g == b == 255) :
-            print(r,g,b)
-
-
+        r, g, b = img.getpixel((j, i))
+        # Delete all white pixels:
+        if not (r == g == b == 255):
+            print(r, g, b)
